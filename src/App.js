3bf762bcf24c1fd2df25bd4658253991aeea61b9
leticiagouvea <letfgouvea@gmail.com>
2023-01-03 22:35:00 -0300

    feat: sign-up and login desktop layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./assets/style/GlobalStyle";
-import SignUp from "./Authentication/SignUp";
+import SignUp from "./pages/Authentication/SignUp";
+import LogIn from "./pages/Authentication/LogIn";
 import Timeline from "./pages/Timeline";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignUp />} />
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/timeline" element={<Timeline />} />
         </Routes>
       </BrowserRouter>
